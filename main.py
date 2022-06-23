39b51e43cb1bfa80163fe4a4cab29a9a241c31f5
Øystein Nerva <oystein.nerva@outlook.com>
2022-06-23 23:14:18 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import importlib
+
+#dd = importlib.import_module('blog_posts.2021 12 - size_and_frequency_SPX_drawdowns')
+korreksjon_i_aksjemarkedene = importlib.import_module("blog_posts.2022 02 - Tracking equity meltdowns")
+
+
+
+
+if __name__ == '__main__':
+    korreksjon_i_aksjemarkedene
