7a1c290446c37c1218b2d2e7931ee60912020056
Øystein <32266633+NRVA@users.noreply.github.com>
2023-04-12 10:23:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ weekdaynum = datetime.today().weekday()
 #dd = importlib.import_module('blog_posts.2021 12 - size_and_frequency_SPX_drawdowns')
 korreksjon_i_aksjemarkedene = importlib.import_module("blog_posts.2022 02 - Tracking equity meltdowns") if datetime.today().weekday() <= 4 else print("No new stock data to get today") # only run on weekdays
 #gavekal_model = importlib.import_module("blog_posts.2020 10 - strukturelle_skift_med_gavekal-modellen")
-notion_dividendscreener = importlib.import_module("myNotionDividendScreener") #if weekdaynum==4 else print(f"Today is day {weekdaynum}: No screener set up for today") # only screen on fridays
+notion_dividendscreener = importlib.import_module("myNotionDividendScreener") 
+#if weekdaynum==4 else print(f"Today is day {weekdaynum}: No screener set up for today") # only screen on fridays
 
 
 
